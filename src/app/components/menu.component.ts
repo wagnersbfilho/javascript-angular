a8837b78eea4505439bcd03fb8e28764a1e10404
Wagner Filho <91101650+wagnersbfilho@users.noreply.github.com>
2023-05-21 12:14:56 +0100

    Delete src directory

--- src/app/components/menu.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'menu-component',
-  template: '<h2>Wagner</h2>',
-  styles: ['h2{ color: blue}'],
-})
-export class MenuComponent {}
--- src/typescript/02-variaveis/datas.ts
+++
@@ -1,4 +0,0 @@
-// padrão americano
-let niver: Date = new Date("2022-12-01 05:00");
-
-console.log(niver.toString())
